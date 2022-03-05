9ebb4f6e727f43d7ce72e293613b6f3346d3095a
AdisonCavani <adriansrodon@tuta.io>
2022-03-05 16:49:51 +0000

    Deploying to gh-pages from @ AdisonCavani/adisoncavani.github.io@709ba3b44d947c43bdfa7d091631f325189f5b5a 🚀

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.toggleMenu = void 0;
 function toggleMenu() {
+    var _a;
     var menuDiv = document.getElementById("menu");
     menuDiv.className = menuDiv.className === "res-menu"
         ? "res-menu-enabled"
         : "res-menu";
+    (_a = document.getElementById("menu-btn")) === null || _a === void 0 ? void 0 : _a.classList.add("btn-menu-enabled");
 }
 exports.toggleMenu = toggleMenu;
 window.onclick = function (e) {
+    var _a;
     if (!document.getElementById("menu-div").contains(e.target)) {
         document.getElementById("menu").className = "res-menu";
+        (_a = document.getElementById("menu-btn")) === null || _a === void 0 ? void 0 : _a.classList.remove("btn-menu-enabled");
     }
 };
