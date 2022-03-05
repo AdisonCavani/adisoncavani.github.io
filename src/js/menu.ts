709ba3b44d947c43bdfa7d091631f325189f5b5a
Adison Cavani <adriansrodon@tuta.io>
2022-03-05 17:49:28 +0100

    Change menu button color if menu is visible

--- src/js/menu.ts
+++ src/js/menu.ts
@@ -5,11 +5,14 @@ export function toggleMenu() {
     menuDiv.className = menuDiv.className === "res-menu"
         ? "res-menu-enabled"
         : "res-menu";
+
+    document.getElementById("menu-btn")?.classList.add("btn-menu-enabled");
 }
 
 // Hide menu if it losed focus
 window.onclick = function (e) {
     if (!document.getElementById("menu-div")!.contains(e.target as Element)) {
         document.getElementById("menu")!.className = "res-menu";
+        document.getElementById("menu-btn")?.classList.remove("btn-menu-enabled");
     }
 }
