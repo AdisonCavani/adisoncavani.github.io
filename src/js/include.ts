bb1d00b38bebb1c54f6f7a2af57e0f6debdf110a
Adison Cavani <adriansrodon@tuta.io>
2022-03-06 23:07:43 +0100

    Add include statement to share elements

---
+++ src/js/include.ts
@@ -0,0 +1,31 @@
+function includeHTML() {
+    var z, i, elmnt: Element, file, xhttp;
+    // Loop through a collection of all HTML elements
+    z = document.getElementsByTagName("include");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        // Search for elements with a certain atrribute
+        file = elmnt.getAttribute("src");
+        if (file) {
+            // Make an HTTP request using the attribute value as the file name
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        var div = document.createElement('div');
+                        div.innerHTML = this.responseText;
+                        elmnt.parentNode?.insertBefore(div.firstChild!, elmnt);
+                        elmnt.parentNode?.removeChild(elmnt);
+                    }
+                    // Remove the attribute, and call this function once more
+                    elmnt.removeAttribute("src");
+                    includeHTML();
+                }
+            };
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            // Exit the function
+            return;
+        }
+    }
+}
