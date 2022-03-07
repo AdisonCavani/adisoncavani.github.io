eda8ee67d5d4b0e513abf55656a1d625735b3e9b
Adrian Środoń <adriansrodon@tuta.io>
2022-03-07 08:06:00 +0100

    Fix theme-toggle not working

--- src/js/theme-toggle.ts
+++ src/js/theme-toggle.ts
@@ -42,11 +42,6 @@ reflectPreference()
 window.onload = () => {
     // set on load so screen readers can see latest value on the button
     reflectPreference()
-
-    // now this script can find and listen for clicks on the control
-    document
-        .querySelector('#theme-toggle')!
-        .addEventListener('click', onClick)
 }
 
 // sync with system changes
