2527735e247cd5595b2072b6143305822432c383
Adison Cavani <adriansrodon@tuta.io>
2022-03-08 00:09:51 +0100

    Fix links href

--- src/js/link.ts
+++ src/js/link.ts
@@ -4,12 +4,7 @@ function init() {
     const menu = document.getElementById("nav-menu");
 
     for (var i = 0; i < menu!.children.length; i++) {
-        // console.log(menu!.children[i]);
-
         if (menu!.children[i].getAttribute("href") == window.location.pathname)
             console.log(`Hit: ${menu!.children[i]}`);
-
-        console.log(menu!.children[i].getAttribute("href"));
     }
-    console.log(window.location.pathname);
 }
