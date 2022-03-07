34fae1b5860b3c1132b764b9a7867dc77a615362
AdisonCavani <adriansrodon@tuta.io>
2022-03-07 23:45:45 +0000

    Deploying to gh-pages from @ AdisonCavani/adisoncavani.github.io@595b10bb9d011ee4e8a7355a7cc8b85dd177c388 🚀

--- js/include.js
+++ js/include.js
@@ -25,6 +25,12 @@ function includeHTML() {
             init();
             return;
         }
-        init();
+    }
+}
+function init() {
+    const menu = document.getElementById("nav-menu");
+    for (var i = 0; i < menu.children.length; i++) {
+        if (menu.children[i].getAttribute("href") == window.location.pathname)
+            menu.children[i].classList.add("menu-entry-current");
     }
 }
--- js/link.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function init() {
-    const menu = document.getElementById("nav-menu");
-    for (var i = 0; i < menu.children.length; i++) {
-        if (menu.children[i].getAttribute("href") == window.location.pathname)
-            menu.children[i].classList.add("menu-entry-current");
-    }
-}
