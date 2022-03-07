595b10bb9d011ee4e8a7355a7cc8b85dd177c388
Adison Cavani <adriansrodon@tuta.io>
2022-03-08 00:45:27 +0100

    TODO: this fn is called multiple times

--- src/js/include.ts
+++ src/js/include.ts
@@ -28,6 +28,14 @@ function includeHTML() {
             // Exit the function
             return;
         }
-        init();
+    }
+}
+
+function init() {
+    const menu = document.getElementById("nav-menu");
+
+    for (var i = 0; i < menu!.children.length; i++) {
+        if (menu!.children[i].getAttribute("href") == window.location.pathname)
+            menu!.children[i].classList.add("menu-entry-current");
     }
 }
--- src/js/link.ts
+++
@@ -1,8 +0,0 @@
-function init() {
-    const menu = document.getElementById("nav-menu");
-
-    for (var i = 0; i < menu!.children.length; i++) {
-        if (menu!.children[i].getAttribute("href") == window.location.pathname)
-            menu!.children[i].classList.add("menu-entry-current");
-    }
-}
