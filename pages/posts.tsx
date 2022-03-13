7e807cae1098e10b5ac0b22e25d0b26038202476
Adison Cavani <adriansrodon@tuta.io>
2022-03-13 19:03:59 +0100

    Add animation to posts page

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -7,13 +7,13 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Container,
   Text,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import BlogPost from "../components/blog-post";
 import { getAllFilesFrontMatter } from "../lib/mdxUtils";
 import Layout from "../components/layouts/article";
+import Section from "../components/section";
 
 export default function Blog({ posts }) {
   const [searchValue, setSearchValue] = useState("");
@@ -51,28 +51,36 @@ export default function Blog({ posts }) {
             maxWidth="700px"
             px={4}
           >
-            <Heading letterSpacing="tight" mb={4} as="h1" size="2xl">
-              Blog
-            </Heading>
-            <Text>
-              I&apos;ve been writing online since 2014, mostly about web
-              development and tech careers. In total, I&apos;ve written 68
-              articles on my blog. Use the search below to filter by title.
-            </Text>
-            <InputGroup mb={4} mr={4} w="100%">
-              <Input
-                aria-label="Search articles"
-                placeholder="Search articles"
-                onChange={(e) => setSearchValue(e.target.value)}
-              />
-              <InputRightElement>
-                <SearchIcon color="gray.300" />
-              </InputRightElement>
-            </InputGroup>
-            {!filteredBlogPosts.length && "No posts found."}
-            {filteredBlogPosts.map((frontMatter) => (
-              <BlogPost key={frontMatter.title} {...frontMatter} />
-            ))}
+            <Section>
+              <Heading letterSpacing="tight" mb={4} as="h1" size="2xl">
+                Blog
+              </Heading>
+            </Section>
+            <Section delay="0.1">
+              <Text>
+                I&apos;ve been writing online since 2014, mostly about web
+                development and tech careers. In total, I&apos;ve written 68
+                articles on my blog. Use the search below to filter by title.
+              </Text>
+            </Section>
+            <Section delay="0.2">
+              <InputGroup mb={4} mr={4} w="100%">
+                <Input
+                  aria-label="Search articles"
+                  placeholder="Search articles"
+                  onChange={(e) => setSearchValue(e.target.value)}
+                />
+                <InputRightElement>
+                  <SearchIcon color="gray.300" />
+                </InputRightElement>
+              </InputGroup>
+            </Section>
+            <Section delay="0.3">
+              {!filteredBlogPosts.length && "No posts found."}
+              {filteredBlogPosts.map((frontMatter) => (
+                <BlogPost key={frontMatter.title} {...frontMatter} />
+              ))}
+            </Section>
           </Flex>
         </Stack>
       </Layout>
