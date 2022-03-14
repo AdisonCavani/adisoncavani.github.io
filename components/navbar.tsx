37b1a27efa9e86d0df9ba518c87575e03be5bcda
Adison Cavani <adriansrodon@tuta.io>
2022-03-14 17:29:54 +0100

    Remove logo from navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,35 +1,35 @@
-import Logo from "./logo";
 import NextLink from "next/link";
 import {
   Container,
   Box,
   Link,
   Stack,
-  Heading,
-  Flex,
   Menu,
   MenuItem,
   MenuList,
   MenuButton,
   IconButton,
   useColorModeValue,
+  Button,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import ThemeToggleButton from "./theme-toggle-button";
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href;
-  const inactiveColor = useColorModeValue("gray200", "whiteAlpha.900");
+  const inactiveColor = useColorModeValue("#686868", "#888");
+  const activeColor = useColorModeValue("#000", "#fff");
 
   return (
     <NextLink href={href} passHref>
-      <Link
+      <Button
         p={2}
-        bg={active ? "glassTeal" : undefined}
-        color={active ? "#202023" : inactiveColor}
+        background="transparent"
+        color={active ? activeColor : inactiveColor}
+        fontWeight={active ? "500" : "regular"}
       >
         {children}
-      </Link>
+      </Button>
     </NextLink>
   );
 };
@@ -55,11 +55,6 @@ const Navbar = (props) => {
         textAlign="center"
         justifyContent="space-between"
       >
-        <Flex align="center" mr={5}>
-          <Heading as="h1" size="lg" letterSpacing={"tighter"}>
-            <Logo />
-          </Heading>
-        </Flex>
         <Stack
           direction={{ base: "column", md: "row" }}
           display={{ base: "none", md: "flex" }}
@@ -68,6 +63,9 @@ const Navbar = (props) => {
           flexGrow={1}
           mt={{ base: 4, nmd: 0 }}
         >
+          <LinkItem href="/" path={path}>
+            Home
+          </LinkItem>
           <LinkItem href="/works" path={path}>
             Works
           </LinkItem>
