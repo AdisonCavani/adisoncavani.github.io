0ee062b31737cd65404178bcb33530795c17f790
Adison Cavani <adriansrodon@tuta.io>
2022-04-01 21:19:51 +0200

    Add better Pagination navigation
    TODO: add dark mode

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -2,40 +2,119 @@ import Link from '@components/Link'
 
 interface Props {
   totalPages: number
+  totalPosts: number
   currentPage: number
 }
 
-export default function Pagination({ totalPages, currentPage }: Props) {
+export default function Pagination({ totalPages, totalPosts, currentPage }: Props) {
   const prevPage = currentPage - 1 > 0
   const nextPage = currentPage + 1 <= totalPages
 
   return (
-    <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-      <nav className="flex justify-between">
-        {!prevPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
-            Previous
-          </button>
-        )}
-        {prevPage && (
-          <Link href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}>
-            <button>Previous</button>
-          </Link>
-        )}
-        <span>
-          {currentPage} of {totalPages}
-        </span>
-        {!nextPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
-            Next
-          </button>
-        )}
-        {nextPage && (
-          <Link href={`/blog/page/${currentPage + 1}`}>
-            <button>Next</button>
-          </Link>
-        )}
-      </nav>
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <MobileNavigation totalPages={totalPages} totalPosts={totalPosts} currentPage={currentPage} />
+      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+        <div>
+          <p className="text-sm text-gray-700">
+            Showing <span className="font-medium">{currentPage}</span> to{' '}
+            <span className="font-medium">{totalPages}</span> of{' '}
+            <span className="font-medium">{totalPosts}</span> posts
+          </p>
+        </div>
+        <div>
+          <nav
+            className="relative z-0 inline-flex -space-x-px rounded-md shadow-sm"
+            aria-label="Pagination"
+          >
+            {prevPage && (
+              <Link
+                href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}
+                className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span className="sr-only">Previous</span>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12.7071 5.29289C13.0976 5.68342 13.0976 6.31658 12.7071 6.70711L9.41421 10L12.7071 13.2929C13.0976 13.6834 13.0976 14.3166 12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L7.29289 10.7071C6.90237 10.3166 6.90237 9.68342 7.29289 9.29289L11.2929 5.29289C11.6834 4.90237 12.3166 4.90237 12.7071 5.29289Z"
+                    fill="#4A5568"
+                  />
+                </svg>
+              </Link>
+            )}
+            <Link
+              href="/"
+              aria-current="page"
+              className="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600"
+            >
+              1
+            </Link>
+            <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
+              ...
+            </span>
+            <Link
+              href="/"
+              className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+            >
+              10
+            </Link>
+            {nextPage && (
+              <Link
+                href={`/blog/page/${currentPage + 1}`}
+                className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50"
+              >
+                <span className="sr-only">Next</span>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M7.29289 14.7071C6.90237 14.3166 6.90237 13.6834 7.29289 13.2929L10.5858 10L7.29289 6.70711C6.90237 6.31658 6.90237 5.68342 7.29289 5.29289C7.68342 4.90237 8.31658 4.90237 8.70711 5.29289L12.7071 9.29289C13.0976 9.68342 13.0976 10.3166 12.7071 10.7071L8.70711 14.7071C8.31658 15.0976 7.68342 15.0976 7.29289 14.7071Z"
+                    fill="#4A5568"
+                  />
+                </svg>
+              </Link>
+            )}
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function MobileNavigation({ totalPages, totalPosts, currentPage }: Props) {
+  const prevPage = currentPage - 1 > 0
+  const nextPage = currentPage + 1 <= totalPages
+
+  return (
+    <div className="flex flex-1 justify-between sm:hidden">
+      {prevPage && (
+        <Link
+          href={currentPage - 1 === 1 ? `/blog/` : `/blog/page/${currentPage - 1}`}
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Previous
+        </Link>
+      )}
+      {nextPage && (
+        <Link
+          href={`/blog/page/${currentPage + 1}`}
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Next
+        </Link>
+      )}
     </div>
   )
 }
